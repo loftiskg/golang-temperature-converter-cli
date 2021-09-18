293e4200ad69de18ee10c9281982d7192bc564c3
Kevin Loftis <loftiskg@gmail.com>
2021-09-18 14:13:10 -0700

    lint

--- main.go
+++ main.go
@@ -25,6 +25,7 @@ func main() {
 		}
 
 		originUnit = strings.ToUpper(os.Args[1])
+
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
 
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
